5d6d0f70a79061e2b6918d556e2bd0f0ffe5c3fc
Ruan Lima <az.88004607@gmail.com>
2026-02-21 19:16:46 -0300

    Atualização da porta SMTP para teste de envio do email

--- utils/configNodeMailer.js
+++ utils/configNodeMailer.js
@@ -3,11 +3,12 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email", // Ou smtp.gmail.com
-    port: 587,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.ETHEREAL_USER,
         pass: process.env.ETHEREAL_PASS,
-    }, 
+    },
     tls: {
         rejectUnauthorized: false
     }
