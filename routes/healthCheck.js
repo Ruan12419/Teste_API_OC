82e531a910c2284a6fd466a848d341f52aa9b109
Ruan Lima <az.88004607@gmail.com>
2026-02-22 19:45:13 -0300

    Fix

--- routes/healthCheck.js
+++ routes/healthCheck.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
  
-app.get('/', (req, res) => res.status(200).json({status: "OK"}))
+router.get('/', (req, res) => res.status(200).json({status: "OK"}))
 
 
 module.exports = router;
